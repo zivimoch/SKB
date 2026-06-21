e8b1f49b32cdd86da98aacde60514b1ce5337e4b
Addzifi Mochamad Gumelar <91119977+zivimoch@users.noreply.github.com>
2026-06-21 13:36:22 +0700

    Revert "feat: sync service logs to Google Sheets"
    This reverts commit 1191ec86f7678a05fbd1de1a7ec848d6d9dab5c8.

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::command('skb:sync-service-log')
-    ->everyFiveMinutes()
-    ->withoutOverlapping()
-    ->when(fn (): bool => (bool) config('google_sheets.enabled'));
